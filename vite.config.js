357d65f7fe44430141dd709464e7e4ad79570c37
Ekta Patel <architectekta@gmail.com>
2025-06-24 22:59:35 -0400

    commit config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/review-dashboard/',
   plugins: [react()],
-  base: '/review-dashboard/', // 👈 repo name
-})
+});
+
